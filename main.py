20df022829b42d01517fb07f180561580155ce5e
jonaquig <jonaquig@woodland.on.ca>
2025-02-03 12:13:33 -0500

    Second

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ x = 100
 print(BLUE + "Enter \'start\' to begin")
 start = input()
 while x == 100:
-   if start == 'start':
-       PE_Start = 0
-       while PE_Start == 0:
-           print(GREEN + "What image would you like to edit? Buffalo, Elephant, or Bird")
-           IMAGE = input()
-           if IMAGE == 'Buffalo' or IMAGE == 'Elephant' or IMAGE == 'Bird':
-               print("Good choice!")
-               if IMAGE == 'Buffalo':
-                   ImageChoice = im
-               elif IMAGE == 'Elephant':
-                   ImageChoice = im1
-               elif IMAGE == 'Bird':
-                   ImageChoice = im2
-               PE_Start = 1
-           elif IMAGE != 'Buffalo' and 'Elephant' and 'Bird':
-               print("You can't do that")
-       if x == 100:
-           x += 1
+    if start == 'start':
+        PE_Start = 0
+        while PE_Start == 0:
+            print(GREEN + "What image would you like to edit? Buffalo, Elephant, or Bird")
+            IMAGE = input()
+            if IMAGE == 'Buffalo' or IMAGE == 'Elephant' or IMAGE == 'Bird':
+                print("Good choice!")
+                if IMAGE == 'Buffalo':
+                    ImageChoice = im
+                elif IMAGE == 'Elephant':
+                    ImageChoice = im1
+                elif IMAGE == 'Bird':
+                    ImageChoice = im2
+                PE_Start = 1
+            elif IMAGE != 'Buffalo' and 'Elephant' and 'Bird':
+                print("You can't do that")
+        if x == 100:
+            x += 1
 
+ImageChoice.show()
